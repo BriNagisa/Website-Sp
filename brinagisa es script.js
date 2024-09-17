b491078ebdd93660e815caa6c63407c192835cce
Brianna Nagisa Rosemary <brihr4@gmail.com>
2024-09-17 20:45:07 +0200

    brinagisa es script.js

---
+++ brinagisa es script.js
@@ -0,0 +1,13 @@
+let nombre = prompt("Introduzca su nombre");
+let genero = prompt("Introduzca su género(M/F)");
+
+if (genero == "M" ){
+alert ("¡Bienvenido " + nombre + "!");
+}
+else if (genero == "F" ){
+alert ("¡Bienvenida " + nombre + "!");
+}
+else { 
+alert ("¡Bienvenide " + nombre + "!")
+}
+
